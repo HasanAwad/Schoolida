a546a546bbc95c8ec4fda11e1c11d6e93efea08c
BayanSwaid <bayan.swaid.sk@gmail.com>
2020-04-21 17:14:55 +0300

    Assignment and Problem classes

---
+++ Assignment.js
@@ -0,0 +1,10 @@
+class Assignment{
+    constructor (teacher, students, problems, file_upload, deadline, grade) {
+        this.teacher = teacher;
+        this.students = students;
+        this.problems = problems;
+        this.file_upload = file_upload;
+        this.deadline = deadline;
+        this.grade = grade;
+    }
+}
